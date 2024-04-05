24a13250675df86504d1c104a24c7eee43608cce
Li Xilong <2057388797@qq.com>
2024-04-05 15:45:52 +0800

    Optimized DOS spectra plotting code.

--- pylinkor.py
+++ pylinkor.py
@@ -3,7 +3,7 @@
     pylinkor module of ChemAuto program.
     Plotting DOS spectra using PES data exported from exporter module
     Developed by: Li Xilong
-    Last Updates：2024-04-01
+    Last Updates：2024-04-05
 '''
 
 import os
@@ -15,8 +15,8 @@ from logcreator import logged_input, logged_print
 import originpro as op
 
 """
-    Last Update: 2024-04-01
-    Author: LXL
+    Last Update: 2024-04-05
+    Author: Li Xilong
     Acknowledgments: Special thanks to ChatGPT-4 for assistance.
     
     -------------------------------------------------------------------------------
@@ -49,13 +49,16 @@ import originpro as op
 
     Example of displaying both short and long names in a window title:
         page.title = 3  # Show both short name and long name
+
+
+
 """
 
 class AutoDOS():
     def __init__(self):
         self.config_file_path = 'chemauto_config.json'
         self.default_config = {
-            "template_name": "D:\\Program Files\\OriginLab\\UserFiles\\DOS-default.otpu",
+            "template_path": "D:\\Program Files\\OriginLab\\UserFiles\\DOS-default.otpu",
             "default_x_range": [0, 3.3],
             "default_y_range": [-0.03, 6.5],
             "show_graph": False
@@ -80,7 +83,7 @@ class AutoDOS():
             json.dump(config, file, indent=4)
         return config
         
-    def process_file(self, curve_file_path=None, line_file_path=None, xlim_values=None):
+    def process_file(self, curve_file_path=None, line_file_path=None, xlim_values=None, exit_origin = True):
         """
             Process the given curve and line files, create graphics and save.
         """
@@ -134,56 +137,69 @@ class AutoDOS():
                 except ValueError:
                     # Transfer failed, input again.
                     logged_print("Invalid input format. Please input two numbers separated by a comma (e.g., 0,3.3).")
-            
-        ylim_values = self.config.get("default_y_range", [0, 6.5])
-        # Example：How to use config
-        template_name = self.config["template_name"]
-        #get dafault value
-        show_graph = self.config.get("show_graph", False)
 
+        # Get parameters and prepare file paths
+        ylim_values = self.config.get("default_y_range", [0, 6.5])
+        template_path = self.config["template_path"]
+        show_graph = self.config.get("show_graph", False)
+        file_name = os.path.basename(os.path.dirname(curve_file_path))
+        file_suffix = file_name.split('-')[-1] if '-' in file_name else file_name
+        ogg_save_path = os.path.join(os.path.dirname(curve_file_path), f"{file_name}.ogg")
+        opju_save_path = os.path.join(os.path.dirname(curve_file_path), f"{file_name}.opju")
+
+        # Start plotting
+        s = time.time()
         op.new()
         op.set_show(show_graph)
-        wks_curve = op.new_sheet('w', hidden=False)
+        wb = op.find_book()
+        wb.lname = f'{file_name}_wb'
+        wb.name = wb.lname
+        wks = op.find_sheet().destroy()
+        wks_curve = wb.add_sheet(name=f'{file_name}_curve')
+        wks_curve.name = f'{file_name}_curve'
+        wks_line = wb.add_sheet(name = f'{file_name}_line')
+        wks_line.name = f'{file_name}_line'
         wks_curve.from_file(curve_file_path)
-        wks_line = op.new_sheet('w', hidden=False)
         wks_line.from_file(line_file_path)
-
-        gp = op.new_graph(template=template_name)
+        gp = op.new_graph(template = template_path)
         gl = gp[0]
         gl.add_plot(wks_curve, coly=1, colx=0, type='l')
         gl.add_plot(wks_line, coly=1, colx=0, type='l')
-        # Set range of x-axis using user input or dafault value
         gl.set_xlim(*xlim_values)
         gl.set_ylim(*ylim_values)
+        gl.lname = f'{file_name}'
+        gl.name = gl.lname
+        gp.lname = f'{file_name}'
+        gp.name = gp.lname
 
-        # Get the name of the folder containing curve data files
-        folder_name = os.path.basename(os.path.dirname(curve_file_path))
-        # Get suffix from folder name
-        folder_suffix = folder_name.split('-')[-1] if '-' in folder_name else folder_name
-        # Construct save path
-        save_path = os.path.join(os.path.dirname(curve_file_path), f"{folder_name}.ogg")
-        
-        # Change the page longname of graph 
-        #gp.lname = f"{folder_name}"
-        
-        # Call labtalk
-        op.lt_exec(f'label -p 10 10 -n l "{folder_suffix}";')
+        # Set properties through .lt_exec method
+        op.lt_exec(f'label -p 10 10 -n l "{file_suffix}";')
         op.lt_exec('l.font=font("Arial");')
         op.lt_exec('l.fsize=30;')
-        # Change longname of graph by page.label
-        op.lt_exec(f'page.label$ = "{folder_name}";')
-        # Page shortname is read-only, changed by win -r command
-        op.lt_exec(f'window -r Graph1 {folder_name};')
-
-        # Property name of layer is shortname
-        op.lt_exec(f'layer.name$ = "{folder_name}";')
-        #op.lt_exec(f'Layer.LongName$ = "123";')
+        op.lt_exec(f'save -ix "{ogg_save_path}";')
+        op.save(f'{opju_save_path}')
+        e = time.time()
+        r = e - s
+        print("Time: {:.2f} seconds".format(r))
+        if exit_origin:
+            op.exit()
         
-        # Name property of page is read-only, can't be changed through following methods
-        #op.lt_exec(f'page.name$ = "{folder_name}";')
-        #op.lt_exec(f'page.lname$ = "{folder_name}";')
-        op.lt_exec(f'save -ix "{save_path}";')
-    
+        '''
+            # Change longname of graph by page.label
+            #op.lt_exec(f'page.label$ = "{file_name}";')
+        
+            # Page shortname is read-only, changed by win -r command
+            #op.lt_exec(f'window -r Graph1 {file_name};')
+        
+            # Property name of layer is shortname
+            #op.lt_exec(f'layer.name$ = "{file_name}";')
+            #op.lt_exec(f'Layer.LongName$ = "123";')
+        
+            # Name property of page is read-only, can't be changed through following methods
+            #op.lt_exec(f'page.name$ = "{file_name}";')
+            #op.lt_exec(f'page.lname$ = "{file_name}";')
+        '''
+
     def process_folder(self, folder_path=None):
         """
             Traverse all subfolders within a given folder, locate and process files ending with *_curve.txt and *_line.txt.
@@ -223,9 +239,9 @@ class AutoDOS():
                 # Transfer failed, input again.
                 logged_print("Invalid input format. Please input two numbers separated by a comma (e.g., 0,3.3).")
 
-        logged_print("Beginning plotting process..\n")
-        start_time = time.time()
+        logged_print("Start plotting, please wait...\n")
 
+        start_time = time.time()
         for root, dirs, files in os.walk(folder_path):
             for dir_name in dirs:
                 sub_folder_path = os.path.join(root, dir_name)
@@ -237,18 +253,18 @@ class AutoDOS():
                     elif file.endswith("_line.txt"):
                         line_file = os.path.join(sub_folder_path, file)
                 if curve_file and line_file:
-                    self.process_file(curve_file, line_file, xlim_values=xlim_values)
-
+                    self.process_file(curve_file, line_file, xlim_values=xlim_values, exit_origin=False)
+        op.exit()
         end_time = time.time()
         run_time = end_time - start_time
-        logged_print(f'Successfully completed! "Total execution time: {run_time} seconds')
+        logged_print(f'Successfully completed! Total execution time: {run_time} seconds')
         
     def run(self):
         start_time = datetime.now()
         logging.info(f"Func 5 started at {start_time}.strftime('%Y-%m-%d %H:%M:%S')")
         
         while True:
-            choice = logged_input("\nChoose an option:\n1. Batch plot DOS\n2. Plot single DOS:\n")
+            choice = logged_input("\nChoose an option:\n1. Batch plot DOS\n2. Plot single DOS\n")
             if choice == '1' or choice == '':
                 # Assuming folder_path is defined or obtained elsewhere in your code
                 result = self.process_folder()  # Call with required argument
